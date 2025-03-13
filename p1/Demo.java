5ec7ad0271155ccd95ad202e0c2bc6641eb776b9
NikhilPatil146 <nikhilp2706@gmail.com>
2025-03-13 09:11:03 +0530

    13-03-2025

---
+++ p1/Demo.java
@@ -0,0 +1,10 @@
+ package com.p1;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		Protection pi=new Protection();
+		Derived d=new Derived();
+		NonDerived n=new NonDerived();
+	}
+}
