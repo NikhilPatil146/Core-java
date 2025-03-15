a83c7dc9e82ccfde2b04edbfa3806920dd265393
NikhilPatil146 <nikhilp2706@gmail.com>
2025-03-15 16:17:09 +0530

    15-03-2025

---
+++ p2/Demo1.java
@@ -0,0 +1,14 @@
+package com.p2;
+
+import com.p1.Derived;
+import com.p1.NonDerived;
+
+
+public class Demo1 {
+
+
+	public static void main(String[] args) {
+		Derived d=new Derived();
+		NonDerived n=new NonDerived();
+	}
+}
